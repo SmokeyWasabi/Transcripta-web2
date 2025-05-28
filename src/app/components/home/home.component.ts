915093ab4f2994613785ebbb0910c4e75562eefb
John Fu <tianlinfu5@gmail.com>
2025-05-27 20:31:35 -0400

    wip

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,14 +6,14 @@ import { ImageModule } from 'primeng/image';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AccordionModule } from 'primeng/accordion';
+import { LanguageService } from '../../service/language.service';
+import { PanelModule } from 'primeng/panel';
 
-// <-- import TranslateModule
 import {
   LangChangeEvent,
   TranslateModule,
   TranslateService,
 } from '@ngx-translate/core';
-import { LanguageService } from '../../service/language.service';
 
 @Component({
   selector: 'app-home',
@@ -26,6 +26,7 @@ import { LanguageService } from '../../service/language.service';
     AnimateOnScrollModule,
     AccordionModule,
     TranslateModule, // <-- add this so the translate pipe is available
+    PanelModule,
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
@@ -34,6 +35,16 @@ import { LanguageService } from '../../service/language.service';
 export class HomeComponent implements OnInit {
   protected voiceToTextGifSrc: string =
     'assets/resource/Enterprise-Grade_Voice-to-Text_EN.gif';
+
+  protected qaItems = [
+    { questionKey: 'QA.QUESTION1', answerKey: 'QA.ANSWER1' },
+    { questionKey: 'QA.QUESTION2', answerKey: 'QA.ANSWER2' },
+    { questionKey: 'QA.QUESTION3', answerKey: 'QA.ANSWER3' },
+    { questionKey: 'QA.QUESTION4', answerKey: 'QA.ANSWER4' },
+    { questionKey: 'QA.QUESTION5', answerKey: 'QA.ANSWER5' },
+    { questionKey: 'QA.QUESTION6', answerKey: 'QA.ANSWER6' },
+    { questionKey: 'QA.QUESTION7', answerKey: 'QA.ANSWER7' },
+  ];
 
   constructor(
     private route: ActivatedRoute,
